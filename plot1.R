40f83c5a74430030071974b1aec60f8a6526bba8
lily013112 <sjylily94@126.com>
2016-11-09 22:52:20 -0800

    Delete plot1.R

--- plot1.R
+++
@@ -1,4 +0,0 @@
-##Plot 1
-hist(data$Global_active_power,xlab="Global Active Power (kilowatts)",main="Global Active Power",col="red")
-dev.copy(png,file="plot1.png",width=480,height=480,units="px")
-dev.off()
